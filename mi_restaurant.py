bc66c80d3a98680eb6c8822601f41556afe87aa7
AlexanderV <alexandervelarde2511@gmail.com>
2023-04-05 15:41:39 -0400

    Creamos todos los paneles y configuramos son 3 paneles principales arriba, izquierda y derecha y sus subpaneles, aun no se puede previsualizar

--- mi_restaurant.py
+++ mi_restaurant.py
@@ -15,8 +15,66 @@ aplicacion.title("Mi restaurante - sistema de facturacion")
 #color de fondo de la ventana
 aplicacion.config(bg='burlywood')
 
+######################################################################
+#panel superior
+panel_superior = Frame(aplicacion, bd=1, relief=FLAT)
+panel_superior.pack(side=TOP)
+
+#etiqueta titulo
+etiqueta_titulo = Label(panel_superior, text='Sistema de Facturacion', fg='azure4', font=('Dosis', 48),
+                        bg='burlywood', width=27)
+
+etiqueta_titulo.grid(row=0, column=0)
 
 
+#########################################################################
+
+#panel menu del lado izquierdo
+panel_izquierdo = Frame(aplicacion, bd=1, relief=FLAT)
+panel_izquierdo.pack(side=LEFT)
+
+####
+
+#panel costos
+panel_costos = Frame(panel_izquierdo, bd=1, relief=FLAT)
+panel_costos.pack(side=BOTTOM)
+
+#panel comida
+panel_comida = LabelFrame(panel_izquierdo, text='Comida', font=('Dosis', 19, 'bold'),
+                          bd=1, relief=FLAT, fg='azure4')
+panel_comida.pack(side=LEFT)
+
+#panel bebidas
+panel_bebida = LabelFrame(panel_izquierdo, text='Bebidas', font=('Dosis', 19, 'bold'),
+                          bd=1, relief=FLAT, fg='azure4')
+panel_bebida.pack(side=LEFT)
+
+#panel Postres
+panel_postres = LabelFrame(panel_izquierdo, text='Postres', font=('Dosis', 19, 'bold'),
+                          bd=1, relief=FLAT, fg='azure4')
+panel_postres.pack(side=LEFT)
+##################################################################################
+
+#panel del lado Derecho
+panel_derecha = Frame(aplicacion, bd=1, relief=FLAT)
+panel_derecha.pack(side=RIGHT)
+
+
+#panel calculadora
+panel_calculadora = Frame(panel_derecha, bd=1, relief=FLAT, bg='burlywood')
+panel_calculadora.pack()
+
+#panel recibo
+panel_recibo = Frame(panel_derecha, bd=1, relief=FLAT, bg='burlywood')
+panel_recibo.pack()
+
+#panel botones
+panel_botones = Frame(panel_derecha, bd=1, relief=FLAT, bg='burlywood')
+panel_botones.pack()
+
+
+
+############################################
 
 #evitar que la pantalla se cierre
 aplicacion.mainloop()
